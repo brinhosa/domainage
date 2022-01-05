ecff893f66b297a924f92d5644f1c2e63b2a9539
Rafael <rafael@brinhosa.com.br>
2022-01-05 08:49:35 -0300

    Update domainage.py
    Read from pipe.

--- domainage.py
+++ domainage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # Name:     Domain Age Calculator
-# By:       Chetan Soni
-# Date:     27 March 2018
+# By:       Chetan Soni (modified by Brinhosa)
+# Date:     05 January 2022
 # ------------------------------------------------------------------
 # Author Website: https://www.yeahhub.com/
 # ------------------------------------------------------------------
@@ -10,7 +10,7 @@
 import os
 import requests
 import json
-
+import sys
 print("""\
  
              \_`-)|_
@@ -29,43 +29,46 @@ CEND = '\033[0m'
 YELLOW = '\033[92m'
 RED = '\033[91m'
    
-enterurl = raw_input(YELLOW + "Enter Your Domain Name (example.com)? " + CEND)
-url = enterurl.split("//")[-1].split("/")[0].split('?')[0]
-show = "https://input.payapi.io/v1/api/fraud/domain/age/" + url
-r = requests.get(show)
+#enterurl = raw_input(YELLOW + "Enter Your Domain Name (example.com)? " + CEND)
 
-os.system('clear')  # on linux / os x
+for line in sys.stdin:
+    sys.stdout.write(line)
+    url = enterurl.split("//")[-1].split("/")[0].split('?')[0]
+    show = "https://input.payapi.io/v1/api/fraud/domain/age/" + url
+    r = requests.get(show)
 
-if r.status_code == 200:
-	print 
-	print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
-	print(RED + "************************************************************" + CEND)
-	print 
+    os.system('clear')  # on linux / os x
+
+    if r.status_code == 200:
+	    print 
+	    print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
+	    print(RED + "************************************************************" + CEND)
+	    print 
 
 	
-	data = r.text
-	jsonToPython = json.loads(data)
+	    data = r.text
+	    jsonToPython = json.loads(data)
 
-	print(YELLOW + "Domain Name: " + CEND + url)
-	print 
-	print (jsonToPython['message'])
+	    print(YELLOW + "Domain Name: " + CEND + url)
+	    print 
+	    print (jsonToPython['message'])
 
-	print 
-	print(RED + "************************************************************" + CEND)
-	print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
-	print 
-else:
-	print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
-	print(RED + "************************************************************" + CEND)
-	print 
+	    print 
+	    print(RED + "************************************************************" + CEND)
+	    print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
+	    print 
+    else:
+	    print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
+	    print(RED + "************************************************************" + CEND)
+	    print 
 
-	print(YELLOW + " - There Is A Problem" + CEND)
-	print(YELLOW + " - Checking The Connection " + CEND)
-	print(YELLOW + " - Enter Website Without HTTP/HTTPS/WWW " + CEND)
-	print(YELLOW + " - Check If Website Working " + CEND)
+	    print(YELLOW + " - There Is A Problem" + CEND)
+	    print(YELLOW + " - Checking The Connection " + CEND)
+	    print(YELLOW + " - Enter Website Without HTTP/HTTPS/WWW " + CEND)
+	    print(YELLOW + " - Check If Website Working " + CEND)
 
-	print 
-	print(RED + "************************************************************" + CEND)
-	print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
-	print 
+	    print 
+	    print(RED + "************************************************************" + CEND)
+	    print(RED + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + CEND)
+	    print 
 
